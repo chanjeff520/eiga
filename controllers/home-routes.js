1503a9e5a8078e660056abe19959c14e110c893b
AMESocker <aesocher@yahoo.com>
2022-11-10 10:41:25 -0800

    added try catch to routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,8 +3,31 @@ const { User, Movie } = require('../models')
 const path = require('path')
 
 router.get('/', async (req, res) => {
-    res.sendFile(path.join(__dirname,'../views/index.html'));
-    console.log("This will show in Terminal");
+    try {
+        res.status(200).sendFile(path.join(__dirname,'../views/index.html'));
+        console.log("This will show in Terminal");
+        
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+
+router.get('/login', async (req, res) => {
+    try {
+        res.status(200).sendFile(path.join(__dirname,'../views/login.html' ))
+        
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+
+router.get('/signup', async (req, res) => {
+    try {
+        res.status(200).sendFile(path.join(__dirname,'../views/signup.html' ))
+        
+    } catch (error) {
+        res.status(500).json(error)
+    }
 });
 
 module.exports = router;
