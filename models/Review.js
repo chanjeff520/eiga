686a7c3bad1163df89b88e3025df1af8909f8df9
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-11-10 13:39:11 -0800

    make seeds.js seed

--- models/Review.js
+++ models/Review.js
@@ -15,22 +15,6 @@ Review.init(
       type: DataTypes.TEXT,
       allowNull: false,
     },
-    user_id: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      references: {
-        model: 'user',
-        key: 'id'
-      }
-    },
-    movie_id: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      references: {
-        model: 'movie',
-        key: 'id'
-      }
-    },
     created_at: {
       type: DataTypes.DATE,
       allowNull: false
