55900abe36ffec554752304d2a77b00abe3d6ff3
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-11-11 02:18:24 -0800

    make tweaks for dynamic background will solve later

--- public/assets/script/homepage.js
+++ public/assets/script/homepage.js
@@ -18,7 +18,7 @@ fetch('/api/movie')
 
 console.log("MEOWWW")
 
-// Automatic Slideshow - change image every 20 seconds
+// Automatic Slideshow - change image every 20 second
 var myIndex = 0;
 carousel();
 
@@ -30,6 +30,7 @@ function carousel() {
   }
   myIndex++;
   if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
+  x[myIndex-1].style.display = "block"; 
+  console.log("22") 
   setTimeout(carousel, 3000);    
 }
