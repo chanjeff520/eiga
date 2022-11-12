b5b49fc63b43fc41ab1fd9868f893426cb816422
Senay Gebrat <senaygebrat@gmail.com>
2022-11-11 19:38:22 -0800

    dynamically created movie cards

--- public/assets/script/movie.js
+++ public/assets/script/movie.js
@@ -1,18 +1,38 @@
 
-let movieData;
+var movieData;
 
 
 //get movie id number into string literal
-fetch(`/api/movie/${window.location.href.split('movie/')[1]}/review`)
-.then((response) => response.json())
-.then((data) => {
-  movieData = data
-  console.log(data)
-});
-console.log(window.location.href.split('movie/')[1])
 
-fetch(`/api/user/${movieData.review[0].user_id}`)
-.then((response) => response.json())
-.then((data) => {
+
+async function getMovieData(){
+  const res = await fetch(`/api/movie/${window.location.href.split('movie/')[1]}/review`)
+  const data = await res.json()
+  console.log(data);
+  generateLeftSide(data);
+}
+
+getMovieData();
+
+async function getReviewData(){
+  const res = await fetch(`/api/user/${movieData.review[0].user_id}`)
+  const data = await res.json()
   console.log(data)
-});
+}
+
+getReviewData();
+
+function generateLeftSide(data){
+  //for title
+  const divEl = document.getElementById('filmTitle');
+  let filmTitle = document.createElement('h2');
+  filmTitle.setAttribute('class', 'ml-5');
+  filmTitle.textContent = data.title;
+  divEl.appendChild(filmTitle);
+  //for movie image under title
+  const imgCont = document.getElementById('imgCont');
+  let filmImage = document.createElement('img');
+  filmImage.setAttribute('class', 'img-thumbnail')
+  filmImage.setAttribute('src', data.image);
+  imgCont.appendChild(filmImage);
+}
