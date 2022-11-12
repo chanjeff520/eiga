82e419a896928eb2d4393e3414ae64534ea38219
AMESocker <aesocher@yahoo.com>
2022-11-11 16:43:59 -0800

    added review to data loaded

--- public/assets/script/movie.js
+++ public/assets/script/movie.js
@@ -1,5 +1,5 @@
 //get movie id number into string literal
-fetch(`/api/movie/${window.location.href.split('movie/')[1]}`)
+fetch(`/api/movie/${window.location.href.split('movie/')[1]}/reviews`)
 .then((response) => response.json())
 .then((data) => console.log(data));
 console.log(window.location.href.split('movie/')[1])
