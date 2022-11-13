b6384e434c07fe11ada9093dbd9316bfdab1134d
AMESocker <aesocher@yahoo.com>
2022-11-13 12:30:10 -0800

    added login function

--- public/assets/script/login.js
+++ public/assets/script/login.js
@@ -23,4 +23,4 @@ const loginFormHandler = async (event) => {
     } 
 };
 
-document.querySelector('.login-form').addEventListener('submit',loginFormHandler)
+document.getElementById('logInButton').addEventListener('click',loginFormHandler)
