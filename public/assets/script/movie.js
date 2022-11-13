786603388a5c849778c5c6918413c8d12bc33d83
chanjeff520 <chanjeff520@gmail.com>
2022-11-12 19:27:23 -0500

    add containers for the rightside

--- public/assets/script/movie.js
+++ public/assets/script/movie.js
@@ -1,3 +1,4 @@
+const { create } = require("../../../models/User");
 
 var movieData;
 
@@ -10,10 +11,12 @@ async function getMovieData(){
   const data = await res.json()
   console.log(data);
   generateLeftSide(data);
+  
 }
 
 getMovieData();
 
+/*
 async function getReviewData(){
   const res = await fetch(`/api/user/${movieData.review[0].user_id}`)
   const data = await res.json()
@@ -21,6 +24,7 @@ async function getReviewData(){
 }
 
 getReviewData();
+*/
 
 function generateLeftSide(data){
   //for title
@@ -28,14 +32,39 @@ function generateLeftSide(data){
   let filmTitle = document.createElement('h2');
   filmTitle.setAttribute('class', 'ml-5');
   filmTitle.textContent = data.title;
-  console.log(data.title)
+  console.log(data.title);
   divEl.appendChild(filmTitle);
   //for movie image under title
   const imgCont = document.getElementById('imgCont');
   let filmImage = document.createElement('img');
-  filmImage.setAttribute('class', 'img-thumbnail')
+  filmImage.setAttribute('class', 'img-thumbnail');
   filmImage.setAttribute('src', data.image);
   imgCont.appendChild(filmImage);
+}
+
+function generateRightSide(data){
+  const reviewCardContainer = document.getElementById('review-section');
+
+  for(let i=0; i<data.review.length; i++){
+    //for the review card
+    let reviewCard = document.createElement('div');
+    reviewCard.setAttribute('class', 'border border-dark rounded');
+
+    //for the title row
+    let titleDiv = document.createElement('div');
+    let titleEl = document.createElement('h3');
+    titleDiv.setAttribute('class', 'row');
+    titleEl = document.setAttribute('class', 'ml-5');
+    titleEl.textContent = data.review[i].title;
+
+    //for the content of inside the review row
+    let reviewDiv = document.createElement('div');
+    let contentContainer = document.createElement('div');
+    let content = document.createElement('p');
+    reviewDiv.setAttribute('class', 'row');
+    contentContainer.setAttribute('class', 'border border-dark rounded')
+
+  }
 }
 
 generateLeftSide();
