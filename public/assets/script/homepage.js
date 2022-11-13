49454b7684c5c0b11b95dca468f98ce9a21b6b68
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-11-12 20:11:04 -0800

    made font to work

--- public/assets/script/homepage.js
+++ public/assets/script/homepage.js
@@ -78,12 +78,12 @@ console.log("MEOWWW")
 
 // Automatic Slideshow - change image every 20 second
 
-// let ghibliArray = ["/assets/background-images/Ghibli_Cagliostro_House_Background.png", "/assets/background-images/Ghibli_Farmland_Background.jpg", "/assets/background-images/Ghibli_Forest_Background.jpg", "/assets/background-images/Ghibli_God_Ray_Forest_Background.png", "/assets/background-images/Ghibli_Howl_Castle_Alps_Background.jpg", "/assets/background-images/Ghibli_Island_Background.jpg", "/assets/background-images/Ghibli_Night_City_Background.jpg", "/assets/background-images/Ghibli_Spirit_Away_Train_Background.png"]
+let ghibliArray = ["/assets/background-images/Ghibli_Cagliostro_House_Background.png", "/assets/background-images/Ghibli_Farmland_Background.jpg", "/assets/background-images/Ghibli_Forest_Background.jpg", "/assets/background-images/Ghibli_God_Ray_Forest_Background.png", "/assets/background-images/Ghibli_Howl_Castle_Alps_Background.jpg", "/assets/background-images/Ghibli_Island_Background.jpg", "/assets/background-images/Ghibli_Night_City_Background.jpg", "/assets/background-images/Ghibli_Spirit_Away_Train_Background.png"]
 
-// let body = document.querySelector("body")
+let body = document.querySelector("body")
 
-// var myIndex = 0;
-// carousel();
+var myIndex = 0;
+carousel();
 
 function carousel() {
   var i;
